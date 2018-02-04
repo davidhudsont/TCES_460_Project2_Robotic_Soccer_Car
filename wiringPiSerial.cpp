28b52d0ec0eed497ae306293f1f03dbe3afd83c3
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2018-02-04 23:42:58 +0000

    Add files via upload

---
+++ wiringPiSerial.cpp
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+int main() {
+	
+	int fd;
+	
+	if ((fd = serialOpen("/dev/ttyS0",115200)) < 0) {
+		fprintf(stderr, "Unable to open serial device : %s\n", strerror (errno));
+		return 1;
+	}
+	if (wiringPi() == -1) {
+		fprintf(stdout, "Unable to start wiringPi: %s\n", strerror (errno));
+		return 1;
+	}
+	while (true) {
+		if (serialDataAvail (fd) {
+			printf("Data recieved is : %s\n", serialGetchar(fd));
+			delay(1000);
+		}
+	}
+	
+	
+}
