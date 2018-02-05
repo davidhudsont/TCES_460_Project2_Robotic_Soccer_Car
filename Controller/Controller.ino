9a29e5d7776628a7bd449f4652ec406087f2a22d
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2018-02-05 22:20:33 +0000

    Add files via upload

---
+++ Controller/Controller.ino
@@ -0,0 +1,26 @@
+
+int horiz = 0;
+int vert = 1;
+
+int joy[2] = {150,75};
+String msg = "";
+void encode() {
+   msg = "";
+   for (int i =0; i<2; i++) {
+       msg = String(msg+joy[i]+"R");
+   }
+   msg = String(msg+"E");
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  encode();
+  // put your main code here, to run repeatedly:
+  Serial.println(msg);
+  
+  delay(1000);
+}
