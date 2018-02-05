9cfe6df3bc02b4c2235f32452ee5d70e2b987ffe
hkdavid74 <32823716+hkdavid74@users.noreply.github.com>
2018-02-05 13:43:35 -0800

    Update car_server.cpp

--- car_server.cpp
+++ car_server.cpp
@@ -1,20 +1,14 @@
-/* A simple server in the internet domain using TCP
-
-   The port number is passed as an argument */
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
-#include <string.h>
-
 #include <unistd.h>
-
-#include <sys/types.h> 
-
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-
 #include <netinet/in.h>
+#include <netdb.h> 
+#include <iostream>
+#include <time.h>
+#include <wiringSerial.h>
 
 
 
